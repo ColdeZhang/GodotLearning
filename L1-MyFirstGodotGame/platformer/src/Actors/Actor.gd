45e3d5d2fa3ef0cd8c4ffba676ce8db1c86ff59d
ColdeZhang <coldezhang@foxmail.com>
2021-06-22 11:58:48 +0800

    20210622

---
+++ L1-MyFirstGodotGame/platformer/src/Actors/Actor.gd
@@ -0,0 +1,18 @@
+"""
+Created by Unlimited_Deer_ on Deer's MBP
+Date: 20200621
+"""
+
+extends KinematicBody2D
+class_name Actor
+
+var velocity: = Vector2.ZERO
+
+export var gravity: = 3000.0
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity)
+
+
---
+++ L1-MyFirstGodotGame/platformer/src/Actors/Player.gd
@@ -0,0 +1,7 @@
+"""
+Created by Unlimited_Deer_ on Deer's MBP
+Date: 20200621
+"""
+
+extends Actor
+
